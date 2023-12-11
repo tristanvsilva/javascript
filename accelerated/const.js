47bcdb5e4c4202938b1dcf22fab33cb3d28abc45
Tristan Silva <tristan.silva@plexigrid.com>
2023-12-11 23:26:19 +0800

    const, let, hoisting
    const, let, hoisting

---
+++ accelerated/const.js
@@ -0,0 +1,7 @@
+const AGE = 30;
+// AGE = 29;
+console.log("AGE - ", AGE)
+
+const NUMBERS = [20, 21, 22];
+NUMBERS.push(23);
+console.log("NUMBERS - ", NUMBERS)
---
+++ accelerated/hoisting.js
@@ -0,0 +1,13 @@
+age = 27;
+console.log(age);
+
+var age;
+// it doesnt work on let and const
+
+function doSomething() {
+    location = 'naga city'
+}
+
+let location;
+doSomething();
+console.log(location)
